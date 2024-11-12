71b761daedfc8c517ac1c68e8b483605f05d2296
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-11-12 19:30:56 -0300

    :wastebasket: files initial removed

---
+++ src/components/CadastroItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>Cadastro de Item</h2>
+    <form @submit.prevent="adicionarItem">
+      <div>
+        <label for="nome">Nome do Item:</label>
+        <input type="text" v-model="nome" id="nome" required />
+      </div>
+
+      <div>
+        <label for="descricao">Descrição:</label>
+        <textarea v-model="descricao" id="descricao" required></textarea>
+      </div>
+
+      <button type="submit">Adicionar Item</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: '',
+      descricao: ''
+    };
+  },
+  methods: {
+    adicionarItem() {
+      const novoItem = {
+        id: Date.now(),
+        nome: this.nome,
+        descricao: this.descricao
+      };
+
+      this.$emit('item-adicionado', novoItem);
+
+      // Limpa os campos do formulário
+      this.nome = '';
+      this.descricao = '';
+    }
+  }
+};
+</script>
